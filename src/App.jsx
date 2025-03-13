d40585b90ece68a556e24fa945b0bef5d6ea2dcc
ajaykumar1298 <ajaykumarak1209@gmail.com>
2025-03-14 01:22:04 +0530

    chang bg color

--- src/App.jsx
+++ src/App.jsx
@@ -37,10 +37,10 @@ localStorage.setItem("todos",JSON.stringify(todos))
 
   return (
    
-   
+    // bg-[#172842]
    <TodoContextProvider value={{todos,addTodo,updateTodo,deleteTodo,doneTodo}}>
-   <div className="bg-[#FFEFD5] min-h-screen py-8" >
-                <div className="bg-[#172842] w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+   <div className="bg-[#1E1B4B] min-h-screen py-8" >
+                <div className=" w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
                     <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
                     <div className="mb-4">
                         {/* Todo form goes here */} 
